3af64c90b645693d6835a2b0c30e86f7d3e7dcc9
Ashutosh Gupta <ashutoshg479@gmail.com>
2023-11-26 10:31:28 +0530

    config

--- 07reactrouter/src/components/index.js
+++ 07reactrouter/src/components/index.js
@@ -5,4 +5,4 @@ import Home from "./Home/Home";
 import Contact from "./Contact/Contact";
 import User from "./User/User";
 
-export {About, Header, Footer, Home, Contact, User};
+export {About, Header, Footer, Home, Contact, User}; 
