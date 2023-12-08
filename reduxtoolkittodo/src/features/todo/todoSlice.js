6896d3f751244e6b2f175311fcf22ab63d825d8d
Ashutosh Gupta <ashutoshg479@gmail.com>
2023-12-09 03:11:33 +0530

    config

---
+++ reduxtoolkittodo/src/features/todo/todoSlice.js
@@ -0,0 +1,30 @@
+import {createSlice, nanoid} from '@reduxjs/toolkit';
+
+const initialState={
+    todos:[{id:1,text:"Hello world"}]
+}
+
+export const todoSlice=createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            const todo={
+                id:nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo);
+        },
+        removeTodo:(state,action)=>{
+            state.todos=state.todos.filter((todo)=>
+            todo.id!=action.payload)
+        },
+        updateTodo:(state,action)=>{
+            const temp=state.todos.findIndex((todo)=>todo.id===action.payload.id)
+            state.todos[temp].text=action.payload.text
+        }
+    }
+}) 
+
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions;
+export default todoSlice.reducers;
